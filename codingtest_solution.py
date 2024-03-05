f7077379e3e73d14971bebad571d461f358d9b38
Linh-An <linhan90@gmail.com>
2024-03-05 21:19:26 +0000

    Optimal solution for coding test

---
+++ codingtest_solution.py
@@ -0,0 +1,43 @@
+# finding the maximum length of a subarray with elements that have difference of at most 1
+# Time complexity: O(n)
+# Space complexity: O(n)
+# Test case: [1, 2, 2, 3, 1, 2] -> 5
+# Test case: [1, 2, 3, 4, 5, 6, 7, 8, 9] -> 2
+# Test case: [1, 1, 1, 1, 1, 1, 1, 1, 1] -> 9
+# Test case: [1, 1, 2, 2, 2, 3, 3, 3, 4, 4] -> 6
+
+
+def find_max_lenght(numbers) -> int:
+    """
+    Finds the maximum length of a subarray with elements that have difference of at most 1.
+    Args:
+        List (list): A list of integers.
+    Returns:
+        int: The maximum length of a subarray with elements that have difference of at most 1.
+    """
+    # create a dictionary to store the count of each element
+    count = {}
+    for num in numbers:
+        if num not in count:
+            count[num] = 1
+        else:
+            count[num] += 1
+    # create a variable to store the maximum length
+    max_length = 0
+    # iterate through the dictionary
+    for k in count:
+        # find the maximum length with previous and next element if they exist
+        max_length = max(
+            max_length,
+            count[k] + count.get(k + 1, 0),
+            count[k] + count.get(k - 1, 0),
+        )
+    # return the maximum length
+    return max_length
+
+
+# test cases
+print(find_max_lenght([1, 2, 2, 3, 1, 2]))  # 5
+print(find_max_lenght([1, 2, 3, 4, 5, 6, 7, 8, 9]))  # 2
+print(find_max_lenght([1, 1, 1, 1, 1, 1, 1, 1, 1]))  # 9
+print(find_max_lenght([1, 1, 2, 2, 2, 3, 3, 3, 4, 4]))  # 6
